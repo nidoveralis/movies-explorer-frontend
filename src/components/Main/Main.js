dfefc6a691e929e3c7b83e617decc6242557b740
Nidoveralis <nidoveralis@gmail.com>
2022-12-12 19:53:37 +0400

    style: desktop

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,9 @@ import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
+import Footer from '../Footer/Footer';
 
 function Main() {
   return (
@@ -10,6 +13,9 @@ function Main() {
         <Promo />
         <AboutProject />
         <Techs />
+        <AboutMe />
+        <Portfolio />
+        <Footer />
       </div>
     </>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import {Route, Switch, Link} from 'react-router-dom';
 function Navigation() {
   return (
     <section className="navigation">
-      <a href="#" className="navigation__signup">Регистрация</a>
+      <a href="/#" className="navigation__signup link">Регистрация</a>
       <button className="navigation__signin">Войти</button>
     </section>
   );
