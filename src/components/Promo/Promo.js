930ca3cef6fcfb2b08459733977a5389cf78a748
Nidoveralis <nidoveralis@gmail.com>
2022-12-13 00:14:54 +0400

    style

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,11 +5,12 @@ import logo_text from '../../images/logo_text.svg';
 function Promo() {
   return (
     <section className="promo">
-      <div className="promo-text">
-        <h1 className="promo-text__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
-        <p className="promo-text__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <button className="promo__button">Узнать больше</button>
+      <div className="promo-content">
+        <h1 className="promo-content__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        <p className="promo-content__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       </div>
+      <button className="promo__button">Узнать больше</button>
+
       <img src={logo_text} className="promo__image"/>
     </section>
   );
