60bad917ac24f71bf233a416192d4758350de713
Nidoveralis <nidoveralis@gmail.com>
2023-01-26 22:04:05 +0400

    fix:added validation for the field email

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,27 +2,40 @@ import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import './Form.css';
 import logo from '../../images/logo.svg';
+import {isValidEmail} from '../../utils/utils';
 
 function Form({ title, submitButton, linkTitle, link, formValues, errServer, linkName }) {
   
   const location = useLocation();
   const [isValidInput, setIsValidInput] = React.useState(false);
+  const [isValidInputEmail, setIsValidInputEmail] = React.useState(false);
   const [isErrorInput, setIsErrorInput] = React.useState({});
   const [formValue,setFormValue] = React.useState({});
-  
-  function nameInputValue(e) {
+  const buttonClass = `element-form__button-submit ${isValidInput && isValidInputEmail ? "" : "element-form__button-submit_error"} `
+
+  function namesInputValue(e) {
     const input = e.target;
     setIsErrorInput({...isErrorInput, [input.name]:input.validationMessage});
-    setFormValue({...formValue, [input.name]:input.value})
+    setFormValue({...formValue, [input.name]:input.value});
     setIsValidInput(e.target.closest('form').checkValidity());
-   }
-   
-  const buttonClass = `element-form__button-submit ${isValidInput ? "" : "element-form__button-submit_error"} `
+   };
+
+   function emailInputValue(e) {
+    const input = e.target;
+    if(isValidEmail(input.value)) {
+      setIsErrorInput({...isErrorInput, [input.name]:input.validationMessage});
+      setFormValue({...formValue, [input.name]:input.value});
+      setIsValidInputEmail(e.target.closest('form').checkValidity());
+    }else {
+      setIsErrorInput({...isErrorInput, [e.target.name]:'Не валидный email'});
+      setIsValidInputEmail(false);
+    };
+   };
 
   function handleSubmit(e) {
     e.preventDefault();
-    formValues(formValue)
-  }
+    formValues(formValue);
+  };
 
   return(
       <section className='element'>
@@ -36,19 +49,19 @@ function Form({ title, submitButton, linkTitle, link, formValues, errServer, lin
           {location.pathname === '/signup' && (
             <fieldset className='element-form__fieldset'>
               <p className='element-form__subtitle'>Имя</p>
-              <input name='name' className={`element-form__input ${isErrorInput.name && "element-form__input_error"}`} type='text' minLength='2' maxLength='40' onChange={nameInputValue} required />
+              <input name='name' className={`element-form__input ${isErrorInput.name && "element-form__input_error"}`} type='text' minLength='2' maxLength='40' onChange={namesInputValue} required />
               <span className={`element-form__span ${isErrorInput.name && "element-form__span_error"} `}>{isErrorInput.name}</span>
             </fieldset>
           )}
           
           <fieldset className='element-form__fieldset'>
             <p className='element-form__subtitle'>E-mail</p>
-            <input name='email' className={`element-form__input ${isErrorInput.mail && "element-form__input_error"}`} type='text' minLength='2' maxLength='40' onChange={nameInputValue} required />
+            <input name='email' className={`element-form__input ${isErrorInput.mail && "element-form__input_error"}`} type='text' onChange={emailInputValue} required />
             <span className={`element-form__span ${isErrorInput.email && "element-form__span_error"} `}>{isErrorInput.email}</span>
           </fieldset>
           <fieldset className='element-form__fieldset'>
             <p className='element-form__subtitle'>Пароль</p>
-            <input name='password' className={`element-form__input ${isErrorInput.password && "element-form__input_error"}`} type='password' onChange={nameInputValue} required />
+            <input name='password' className={`element-form__input ${isErrorInput.password && "element-form__input_error"}`} type='password' onChange={namesInputValue} required />
             <span className={`element-form__span ${isErrorInput.password && "element-form__span_error"} `}>{isErrorInput.password}</span>
           </fieldset>
           <div className='element-form__buttons' >
