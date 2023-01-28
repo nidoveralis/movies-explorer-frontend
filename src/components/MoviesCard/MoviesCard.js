5038d04e052719dfc23075bf642ff42df578b4d0
Nidoveralis <nidoveralis@gmail.com>
2023-01-28 16:50:36 +0400

    feat: added a function that checks if the user has this movie

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,30 +3,36 @@ import { useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 import logoCardSaved from '../../images/logo__saved.svg';
 import logoCardDelete from '../../images/logo__cardDel.svg';
-import config from '../../utils/utils';
 
-function MoviesCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, movieId, nameRU, nameEN, statusSeved, userId, clickCard }) {
-  
+function MoviesCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, id, movieId, nameRU, nameEN, clickCard, removeCard, cards }) {
+
   const location = useLocation();
-  const [statusSaved, setStatusSaved] = React.useState(false);
+  const [statusSaved, setStatusSaved] = React.useState(cards.some(item=>item.movieId === movieId));
 
-  const buttonCardClass = `moviesCard-button ${statusSaved ? "moviesCard-button_saved" : ""}`
+  const buttonCardClass = `moviesCard-button ${statusSaved? "moviesCard-button_saved" : ""}`
   const buttonCardContentTitle = <p className='moviesCard-button__title'>Сохранить</p>;
   const buttonCardContentSave = <img className='moviesCard-button__logo-saved' src={logoCardSaved} alt='Фильм сохранён' />;
   const buttonCardContentRemove = <img className='moviesCard-button__logo-saved' src={logoCardDelete} alt='Удалить фильм' />;
   const time = Math.trunc(duration / 60) ? `${Math.trunc(duration / 60)}ч ${duration % 60}м` : `${duration}м`;
-
-  function getCard() {
-    setStatusSaved(true)
-    clickCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, movieId, nameRU, nameEN })
-  }
-
   
+  function getCard() {
+    if(statusSaved) {
+      setStatusSaved(false);
+      removeCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, id, movieId, nameRU, nameEN });
+    }else {
+      setStatusSaved(true);
+      clickCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, id, movieId, nameRU, nameEN });
+    }
+  };
+
+  function removeSavedFilm() {
+      clickCard({ country, director, duration, year, description, image, trailerLink, thumbnail, owner, id, movieId, nameRU, nameEN });
+  };
 
   return (
     <div className='moviesCard'>
       {location.pathname === '/saved-movies' && (
-        <button className='moviesCard-button moviesCard-button_remove' onClick={getCard}>{buttonCardContentRemove}
+        <button className='moviesCard-button moviesCard-button_remove' onClick={removeSavedFilm}>{buttonCardContentRemove}
         </button>
       )}
       {location.pathname === '/movies' && (
